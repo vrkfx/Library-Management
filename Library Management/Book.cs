7620ce440af19f46095297bc36b9a0f8f1fb7e15
Virroshan_Kumar <virroshan08@outlook.com>
2024-03-07 21:46:25 +1030

    Added Basic Methods for book class and library class

--- Library Management/Book.cs
+++ Library Management/Book.cs
@@ -8,5 +8,25 @@ namespace Library_Management
 {
     internal class Book
     {
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String ISBN { get; set; }
+
+        public Book(String title, String author, String isbn) 
+        {
+            this.Title = title;
+            this.Author = author;
+            this.ISBN = isbn;
+        }
+
+        public void AbouBook()
+        {
+            Console.WriteLine($" Book Title: {Title} \n Author: {Author} \n ISBN: {ISBN} \n Genre: {Genre}");
+        }
+
+        public void GetAllBooksByName() 
+        {
+            Console.WriteLine($" Book Title: {Title}");
+        }
     }
 }
--- Library Management/Library.cs
+++ Library Management/Library.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Library_Management
 {
     internal class Library
     {
+        public List<Book> books;
+
+        public Library() 
+        {
+            books = new List<Book> ();
+        }
+
+        public void AddBook(Book book) 
+        {
+            books.Add(book);
+            Console.WriteLine($" '{book.Title}' has been Added to Library ");
+        }
+
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+            Console.WriteLine($" '{book.Title}' has been removed from library");
+        }
+
+        public void DisplayAll() 
+        { 
+            foreach (Book book in books)
+            {
+                book.GetAllBooksByName();
+            }
+        }
+
+        public Book SearchBook(String title) {
+
+            return books.Find(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        }
     }
 }
